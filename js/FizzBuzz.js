bb94b356fdec8da75767569200e8964a2ec75380
marcin walendzik <marcin.walendzik@gmail.com>
2015-01-20 12:27:18 +0000

    private method

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -2,14 +2,20 @@ var FizzBuzz = function() {
   //this.color = "red";
 }; //like object def
 
+
+FizzBuzz.prototype._isDivisibleBy = function(number,divisor){
+  return (number % divisor === 0);
+};
+
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
-  return number % 3 === 0;
+  return this._isDivisibleBy(number,3);
 };
 
 FizzBuzz.prototype.isDivisibleByFive = function(number) {
-  return number %5 === 0;
+  return this._isDivisibleBy(number,5);
 };
 
 FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
-  return number % 15 === 0;
+  return this._isDivisibleBy(number,15);
 };
+
--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,38 +1,35 @@
 describe("",function() {
   
-  var fizzbuzz
+  var fizzbuzz;
+
+  beforeEach(function() {
+    fizzbuzz = new FizzBuzz();
+  });
 
   describe("know when a number",function() {
 
     it("is divisible by 3",function() {
-      fizzbuzz = new FizzBuzz();
-
       //fizbuzz = FizzBuzz.new in ruby
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it("is NOT divisible by 3",function(){
-      fizzubuz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(2)).toBe(false);
     });
 
     it("is divisible by 5",function(){
-      fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByFive(10)).toBe(true);
     });
 
     it("is NOT divisible by 5",function(){
-      fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByFive(9)).toBe(false);
     });
 
     it("is divisible by 15",function(){
-      fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
     it("is NOT divisible by 15",function(){
-      fizzbuzz = new FizzBuzz();
       expect(fizzbuzz.isDivisibleByFifteen(10)).toBe(false);
     });
 
